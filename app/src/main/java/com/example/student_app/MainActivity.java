25b022eb46fcb97bf331d31097018a0cd6dabc6c
Vishal <vishalkarthikeyan17@gmail.com>
2020-11-08 00:39:01 +0530

    initial commit

---
+++ app/src/main/java/com/example/student_app/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.student_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button Login_Button;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Login_Button= findViewById(R.id.loginBtn);
+
+        Login_Button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent loginToHome=new Intent(MainActivity.this,Home_Page.class);
+                startActivity(loginToHome);
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/student_app/Next_Review.java
@@ -0,0 +1,14 @@
+package com.example.student_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Next_Review extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_next__review);
+    }
+}
